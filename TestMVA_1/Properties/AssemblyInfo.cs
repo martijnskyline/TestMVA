c646239e3f57362e7a0a63bc1d43e1dd1ad6bc2a
martijnskyline <109655483+martijnskyline@users.noreply.github.com>
2023-03-23 10:02:18 +0100

    Add project files.

---
+++ TestMVA_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMVA_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestMVA_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6E72B457-66F6-4BE5-A212-A9EC76A2486A")]
+[assembly: AssemblyVersion("1.0.0.0")]
